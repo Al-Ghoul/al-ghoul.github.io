ed2421aa8766284d1fd30a55271efaa27316a428
Abdo .AlGhoul <Abdo.AlGhouul@gmail.com>
2025-08-30 16:56:23 +0300

    feat: add fluid cursor effect

--- app/src/app/layout.tsx
+++ app/src/app/layout.tsx
@@ -1,7 +1,7 @@
-
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import FluidCanvas from "@/components/FluidCanvas";
 import { Navbar } from "@/components/ui/Navbar";
 
 const geistSans = Geist({
@@ -31,6 +31,7 @@ export default function RootLayout({
       >
         <Navbar />
         {children}
+        <FluidCanvas />
       </body>
     </html>
   );
