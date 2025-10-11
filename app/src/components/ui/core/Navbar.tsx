30841f4894911936c66bdfc6d486b8ce6d6254ba
Abdo .AlGhoul <Abdo.AlGhouul@gmail.com>
2025-10-11 23:17:54 +0300

    feat: add nav bar

---
+++ app/src/components/ui/core/Navbar.tsx
@@ -0,0 +1,84 @@
+import {
+  NavbarWrapper,
+  NavBody,
+  NavItems,
+  MobileNav,
+  NavbarLogo,
+  MobileNavHeader,
+  MobileNavToggle,
+  MobileNavMenu,
+  NavbarLinkButton,
+} from "../resizeable-navbar";
+import { useState } from "react";
+import { IconBrandGithub } from "@tabler/icons-react";
+
+export function Navbar({
+  locale,
+  logo,
+  navItems,
+  githubText
+}: {
+  locale: string;
+  logo: string;
+  navItems: {
+    name: string;
+    link: string;
+  }[];
+  githubText: string
+}
+) {
+  const TEXT_DIRECTION = locale === "ar" ? "rtl" : "ltr";
+
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
+  return (
+    <NavbarWrapper>
+      {/* Desktop Navigation */}
+      <NavBody>
+        <NavbarLogo title={logo} />
+        <NavItems items={navItems} />
+        <div className="flex items-center gap-4 relative z-30">
+          { /* <LanguageSelector /> */}
+          <NavbarLinkButton url="https://github.com/Al-Ghoul/al-ghoul" dir={TEXT_DIRECTION}>
+            <IconBrandGithub className="text-white dark:text-black" />
+            <span>{githubText}</span>
+          </NavbarLinkButton>
+        </div>
+      </NavBody>
+
+      {/* Mobile Navigation */}
+      <MobileNav>
+        <MobileNavHeader>
+          <NavbarLogo title={logo} />
+          <MobileNavToggle
+            isOpen={isMobileMenuOpen}
+            onClick={() => setIsMobileMenuOpen(!isMobileMenuOpen)}
+          />
+        </MobileNavHeader>
+
+        <MobileNavMenu
+          isOpen={isMobileMenuOpen}
+          onClose={() => setIsMobileMenuOpen(false)}
+        >
+          {navItems.map((item, idx) => (
+            <a
+              key={`mobile-link-${idx}`}
+              href={item.link}
+              onClick={() => setIsMobileMenuOpen(false)}
+              className="relative text-neutral-600 dark:text-neutral-300"
+            >
+              <span className="block">{item.name}</span>
+            </a>
+          ))}
+          <div className="flex w-full flex-col gap-4">
+            { /* <LanguageSelector /> */}
+            <NavbarLinkButton className="justify-center" url="https://github.com/Al-Ghoul/al-ghoul">
+              <IconBrandGithub className="text-white dark:text-black" />
+              <span>{githubText}</span>
+            </NavbarLinkButton>
+          </div>
+        </MobileNavMenu>
+      </MobileNav>
+    </NavbarWrapper>
+  );
+}
---
+++ app/src/components/ui/resizeable-navbar.tsx
@@ -0,0 +1,270 @@
+import { cn } from "../../lib/utils";
+import { IconMenu2, IconX, IconHome } from "@tabler/icons-react";
+import {
+  motion,
+  AnimatePresence,
+  useScroll,
+  useMotionValueEvent,
+} from "motion/react";
+import React, { type ComponentProps, useEffect, useRef, useState } from "react";
+
+export const NavbarWrapper = ({ children, className, isSearchOpen }: NavbarProps) => {
+  const ref = useRef<HTMLDivElement>(null);
+  const { scrollY } = useScroll({
+    target: ref,
+    offset: ["start start", "end start"],
+  });
+  const [visible, setVisible] = useState<boolean>(false);
+
+  useMotionValueEvent(scrollY, "change", (latest) => {
+    setVisible(!isSearchOpen && latest > 50);
+  });
+
+  useEffect(() => {
+    if (isSearchOpen) {
+      setVisible(false);
+    } else {
+      const latest = scrollY.get();
+      setVisible(latest > 50);
+    }
+  }, [isSearchOpen, scrollY]);;
+
+  return (
+    <motion.div
+      ref={ref}
+      className={cn("fixed inset-x-0 top-2 z-40 w-full", className)}
+      initial={{ y: -50, }}
+      animate={{ y: 5, }}
+      exit={{ y: -50, }}
+      transition={{ duration: 0.5 }}
+    >
+      {React.Children.map(children, (child) =>
+        React.isValidElement(child)
+          ? React.cloneElement(
+            child as React.ReactElement<{ visible?: boolean }>,
+            { visible },
+          )
+          : child,
+      )}
+    </motion.div>
+  );
+};
+
+export const NavBody = ({ children, className, visible }: NavBodyProps) => {
+  return (
+    <motion.div
+      animate={{
+        backdropFilter: visible ? "blur(10px)" : "none",
+        boxShadow: visible
+          ? "0 0 24px rgba(34, 42, 53, 0.06), 0 1px 1px rgba(0, 0, 0, 0.05), 0 0 0 1px rgba(34, 42, 53, 0.04), 0 0 4px rgba(34, 42, 53, 0.08), 0 16px 68px rgba(47, 48, 55, 0.05), 0 1px 0 rgba(255, 255, 255, 0.1) inset"
+          : "none",
+        width: visible ? "40%" : "100%",
+        y: visible ? 20 : 0,
+      }}
+      transition={{
+        type: "spring",
+        stiffness: 200,
+        damping: 50,
+      }}
+      style={{
+        minWidth: "800px",
+      }}
+      className={cn(
+        "relative z-[60] mx-auto hidden w-full max-w-7xl flex-row items-center justify-between self-start rounded-full px-4 py-2 lg:flex bg-white/80 dark:bg-black/80 backdrop-filter backdrop-blur-sm",
+        visible && "bg-white/30 dark:bg-black/30",
+        className,
+      )}
+    >
+      {children}
+    </motion.div>
+  );
+};
+
+export const NavItems = ({ items, className, onItemClick }: NavItemsProps) => {
+  const [hovered, setHovered] = useState<number | null>(null);
+
+  return (
+    <motion.div
+      onMouseLeave={() => setHovered(null)}
+      className={cn(
+        "absolute inset-0 hidden flex-1 flex-row items-center justify-center space-x-2 text-sm font-medium text-zinc-600 transition duration-200 hover:text-zinc-800 lg:flex lg:space-x-2",
+        className,
+      )}
+    >
+      {items.map((item, idx) => (
+        <a
+          onMouseEnter={() => setHovered(idx)}
+          onClick={onItemClick}
+          className="relative px-4 py-2 text-neutral-600 dark:text-neutral-300"
+          key={`link-${idx}`}
+          href={item.link}
+        >
+          {hovered === idx && (
+            <motion.div
+              layoutId="hovered"
+              className="absolute inset-0 h-full w-full rounded-full bg-gray-100 dark:bg-neutral-800"
+            />
+          )}
+          <span className="relative z-20">{item.name}</span>
+        </a>
+      ))}
+    </motion.div>
+  );
+};
+
+export const MobileNav = ({ children, className, visible }: MobileNavProps) => {
+  return (
+    <motion.div
+      animate={{
+        backdropFilter: visible ? "blur(10px)" : "none",
+        boxShadow: visible
+          ? "0 0 24px rgba(34, 42, 53, 0.06), 0 1px 1px rgba(0, 0, 0, 0.05), 0 0 0 1px rgba(34, 42, 53, 0.04), 0 0 4px rgba(34, 42, 53, 0.08), 0 16px 68px rgba(47, 48, 55, 0.05), 0 1px 0 rgba(255, 255, 255, 0.1) inset"
+          : "none",
+        width: visible ? "90%" : "100%",
+        paddingRight: visible ? "12px" : "0px",
+        paddingLeft: visible ? "12px" : "0px",
+        borderRadius: visible ? "4px" : "2rem",
+        y: visible ? 20 : 0,
+      }}
+      transition={{
+        type: "spring",
+        stiffness: 200,
+        damping: 50,
+      }}
+      className={cn(
+        "relative z-50 mx-auto flex w-full max-w-[calc(100vw-2rem)] flex-col items-center justify-between bg-transparent px-0 py-2 lg:hidden",
+        visible && "bg-white/80 dark:bg-black/80",
+        className,
+      )}
+    >
+      {children}
+    </motion.div>
+  );
+};
+
+export const MobileNavHeader = ({
+  children,
+  className,
+}: MobileNavHeaderProps) => {
+  return (
+    <div
+      className={cn(
+        "flex w-full flex-row items-center justify-between",
+        className,
+      )}
+    >
+      {children}
+    </div>
+  );
+};
+
+export const MobileNavMenu = ({
+  children,
+  className,
+  isOpen,
+}: MobileNavMenuProps) => {
+  return (
+    <AnimatePresence>
+      {isOpen && (
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+          className={cn(
+            "absolute inset-x-0 top-16 z-50 flex w-full flex-col items-start justify-start gap-4 rounded-lg px-4 py-8 shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset] bg-white/50 dark:bg-black/50 backdrop-filter backdrop-blur-sm",
+            className,
+          )}
+        >
+          {children}
+        </motion.div>
+      )}
+    </AnimatePresence>
+  );
+};
+
+export const MobileNavToggle = ({
+  isOpen,
+  onClick,
+}: {
+  isOpen: boolean;
+  onClick: () => void;
+}) => {
+  return isOpen ? (
+    <IconX className="text-black dark:text-white" onClick={onClick} />
+  ) : (
+    <IconMenu2 className="text-black dark:text-white" onClick={onClick} />
+  );
+};
+
+export const NavbarLogo = ({ title }: { title: string }) => {
+  return (
+    <a
+      href="/"
+      className="flex space-x-2 text-sm font-normal"
+    >
+      <IconHome className="text-black dark:text-white" />
+      <span className="font-bold text-lg my-auto text-black dark:text-white">{title}</span>
+    </a>
+  );
+};
+
+export function NavbarLinkButton({
+  children,
+  className,
+  url = "#",
+  ...props
+}:
+  {
+    children: React.ReactNode,
+    className?: string,
+    url?: string,
+  } & ComponentProps<"a">) {
+  return (
+    <a
+      className={cn("flex items-center px-2 py-2 rounded-lg bg-white text-black text-sm font-bold relative cursor-pointer hover:-translate-y-0.5 transition duration-200 text-center", className)}
+      rel="noopener noreferrer"
+      {...props}
+    >
+      {children}
+    </a>
+  );
+}
+
+interface NavbarProps {
+  children: React.ReactNode;
+  className?: string;
+  isSearchOpen?: boolean;
+}
+
+interface NavBodyProps {
+  children: React.ReactNode;
+  className?: string;
+  visible?: boolean;
+}
+
+interface NavItemsProps {
+  items: {
+    name: string;
+    link: string;
+  }[];
+  className?: string;
+  onItemClick?: () => void;
+}
+
+interface MobileNavProps {
+  children: React.ReactNode;
+  className?: string;
+  visible?: boolean;
+}
+
+interface MobileNavHeaderProps {
+  children: React.ReactNode;
+  className?: string;
+}
+
+interface MobileNavMenuProps {
+  children: React.ReactNode;
+  className?: string;
+  isOpen: boolean;
+  onClose: () => void;
+}
