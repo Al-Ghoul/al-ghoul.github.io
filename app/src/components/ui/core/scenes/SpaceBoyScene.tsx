1afbf3d2553a8b0c4a5e5911e1c06e5355fd492c
Abdo .AlGhoul <Abdo.AlGhouul@gmail.com>
2025-10-14 13:48:17 +0300

    feat: add post content page

---
+++ app/src/components/ui/core/scenes/SpaceBoyScene.tsx
@@ -0,0 +1,85 @@
+import { Canvas, useFrame, useThree } from "@react-three/fiber";
+import { EffectComposer, Bloom } from "@react-three/postprocessing";
+import * as THREE from "three";
+import { useEffect, useRef } from "react";
+import { useGLTF } from "@react-three/drei";
+
+function ResponsiveCamera() {
+  const { camera, size } = useThree();
+
+  useEffect(() => {
+    const cam = camera as THREE.PerspectiveCamera;
+
+    if (size.width < 1024) {
+      cam.fov = 125;
+      cam.position.set(-0.1, 0.6, 3.2);
+      cam.lookAt(1.2, 0.25, 0.2);
+    } else {
+      cam.fov = 75;
+      cam.position.set(-0.02, 0.45, 2.32);
+      cam.lookAt(1.51, 0.30, 0.30);
+    }
+
+    cam.updateProjectionMatrix();
+  }, [camera, size]);
+
+  return null;
+}
+
+
+export default function SpaceBoyScene() {
+  return (
+    <Canvas className="w-full h-full">
+      <Scene />
+    </Canvas>
+  );
+}
+
+function Scene() {
+  const { scene, animations } = useGLTF(
+    "/space_boy_scene.glb"
+  );
+  const mixer = useRef<THREE.AnimationMixer | null>(null);
+
+  useEffect(() => {
+    mixer.current = new THREE.AnimationMixer(scene);
+    animations.forEach((clip) => {
+      const action = mixer.current?.clipAction(clip);
+      action?.reset().play();
+    });
+    return () => {
+      mixer.current?.stopAllAction();
+    };
+  }, [animations, scene]);
+
+  useFrame((_state, delta) => {
+    mixer.current?.update(delta);
+  });
+
+  return (
+    <>
+      <primitive object={scene} />
+
+      <spotLight
+        position={[-0.2, 0, 2.3]}
+        angle={0.5}
+        penumbra={0.3}
+        intensity={20}
+        castShadow
+        color="#ff9044"
+      />
+
+      <directionalLight intensity={0.5} />
+
+      <EffectComposer>
+        <Bloom
+          intensity={1.5}
+          luminanceThreshold={0.1}
+          luminanceSmoothing={0.9}
+        />
+      </EffectComposer>
+
+      <ResponsiveCamera />
+    </>
+  );
+}
---
+++ app/src/components/ui/posts/PostContent.tsx
@@ -0,0 +1,84 @@
+import { cn } from "@lib/utils";
+import {
+  motion,
+} from "motion/react";
+import { format } from 'date-fns'
+import type { CollectionEntry } from "astro:content";
+import { ar } from "date-fns/locale";
+
+interface PostContentProps {
+  post: CollectionEntry<"posts">;
+  className?: string;
+  locale: string;
+  children?: React.ReactNode;
+}
+
+export default function PostContent({
+  post,
+  className,
+  locale = "ar",
+  children,
+}: PostContentProps) {
+  const TEXT_DIRECTION = post.data.lang === "ar" ? "rtl" : "ltr";
+
+  return (
+    <motion.article
+      className={cn(
+        "prose prose-zinc prose-invert font-jetbrains [font-variant-ligatures:none] p-4 max-w-4xl py-8 bg-white/5 dark:bg-black/50 backdrop-filter backdrop-blur-sm rounded-xl shadow-lg border",
+        className
+      )}
+      initial={{
+        y: 50,
+        opacity: 0,
+        transform: "translate3d(0, 80px, 0) scale(0.9) skewY(-5deg)",
+      }}
+      animate={{
+        y: -55,
+        opacity: 1,
+        transform: "translate3d(0, 0, 0) scale(1) skewY(0deg)",
+      }}
+      exit={{ y: 50 }}
+      transition={{
+        duration: 1.2,
+        ease: [0.22, 1, 0.36, 1],
+      }}
+    >
+      <div className="mb-8 text-center">
+        <time dateTime={post.data.publishedAt.toDateString()} className="block text-xs text-muted-foreground" dir={TEXT_DIRECTION}>
+          {locale == "ar" ?
+            format(post.data.publishedAt, "d, LLLL yyyy", { locale: ar })
+            :
+            format(post.data.publishedAt, "LLLL d, yyyy")}
+        </time>
+
+        <h1 className="text-3xl font-bold mt-1" dir={TEXT_DIRECTION}>{post.data.title}</h1>
+
+        {post.data.tags?.length > 0 ? (
+          <div className="flex flex-wrap gap-2 justify-center">
+            {post.data.tags.map(({ id }) => (
+              <span
+                key={id}
+                className="px-2 py-0.5 rounded-full bg-secondary/30 font-medium text-xs"
+              >
+                #{id}
+              </span>
+            ))}
+          </div>
+        ) : null}
+
+        <div className="mt-3 flex flex-wrap items-center justify-center gap-3 text-sm text-muted-foreground">
+          {post.data.category ? (
+            <span className="px-2 py-0.5 rounded-full bg-primary/10 text-primary font-medium text-xs">
+              {post.data.category.id}
+            </span>
+          ) : null}
+        </div>
+      </div>
+
+      <div dir={post.data.lang === "ar" ? "rtl" : "ltr"} lang={post.data.lang}>
+        {children}
+      </div>
+
+    </motion.article>
+  )
+}
