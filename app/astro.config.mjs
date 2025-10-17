1465b9cccc9b4b5c32bb46204a0ff02c6edcf689
Abdo .AlGhoul <Abdo.AlGhouul@gmail.com>
2025-10-17 04:28:35 +0300

    feat: setup astro's sitemap & robots.txt

--- app/astro.config.mjs
+++ app/astro.config.mjs
@@ -10,12 +10,26 @@ import rehypeSlug from 'rehype-slug';
 import rehypeAutolinkHeadings from 'rehype-autolink-headings';
 import { remarkRelatedTitles } from './src/plugins/remark-related-titles.mjs';
 
+import sitemap from '@astrojs/sitemap';
+
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://al-ghoul.github.io",
-  integrations: [react(), mdx({
-    remarkPlugins: [remarkRelatedTitles],
-  })],
+  integrations: [
+    react(), mdx({
+      remarkPlugins: [remarkRelatedTitles],
+    }),
+    sitemap({
+      i18n: {
+        defaultLocale: 'ar',
+        locales: {
+          ar: 'ar',
+          en: 'en',
+        },
+      },
+    })
+  ],
 
   i18n: {
     defaultLocale: 'ar',
