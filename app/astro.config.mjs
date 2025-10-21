b0efec88061a7ab16f17a5521d9de8957b4f81c1
Abdo .AlGhoul <Abdo.AlGhouul@gmail.com>
2025-10-21 06:53:38 +0300

    feat: add shiki transformers, copy-button & colorized-brackets

--- app/astro.config.mjs
+++ app/astro.config.mjs
@@ -14,6 +14,16 @@ import sitemap from '@astrojs/sitemap';
 import { remarkExcerpt } from './src/plugins/remark-excerpt.mjs';
 import { remarkReadingTime } from './src/plugins/remark-reading-time.mjs';
 
+import { transformerCopyButton } from '@selemondev/shiki-transformer-copy-button';
+import { transformerColorizedBrackets } from '@shikijs/colorized-brackets';
+import {
+  transformerNotationDiff,
+  transformerNotationErrorLevel,
+  transformerNotationFocus,
+  transformerNotationHighlight,
+  transformerNotationWordHighlight,
+} from '@shikijs/transformers';
+
 
 // https://astro.build/config
 export default defineConfig({
@@ -41,6 +51,22 @@ export default defineConfig({
   markdown: {
     shikiConfig: {
       theme: 'vitesse-black',
+      transformers: [
+        transformerColorizedBrackets(),
+        transformerNotationHighlight(),
+        transformerCopyButton({
+          duration: 3000,
+          display: 'ready',
+          successIcon: `data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' fill='none' stroke='rgba(128,128,128,1)' stroke-linecap='round' stroke-linejoin='round' stroke-width='2' viewBox='0 0 24 24'%3E%3Crect width='8' height='4' x='8' y='2' rx='1' ry='1'/%3E%3Cpath d='M16 4h2a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2h2'/%3E%3Cpath d='m9 14 2 2 4-4'/%3E%3C/svg%3E`,
+          copyIcon: `data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' fill='none' stroke='rgba(128,128,128,1)' stroke-linecap='round' stroke-linejoin='round' stroke-width='2' viewBox='0 0 24 24'%3E%3Crect width='8' height='4' x='8' y='2' rx='1' ry='1'/%3E%3Cpath d='M16 4h2a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2h2'/%3E%3C/svg%3E`,
+        }),
+        transformerNotationDiff({
+          matchAlgorithm: 'v3',
+        }),
+        transformerNotationWordHighlight(),
+        transformerNotationFocus(),
+        transformerNotationErrorLevel(),
+      ],
     },
 
     rehypePlugins: [
