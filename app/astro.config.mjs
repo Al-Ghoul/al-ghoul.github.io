1c485b172e4f9972cf6bdc22b16f7945a4c3903a
Abdo .AlGhoul <Abdo.AlGhouul@gmail.com>
2025-10-21 07:26:29 +0300

    feat: add remark-gfm

--- app/astro.config.mjs
+++ app/astro.config.mjs
@@ -14,6 +14,7 @@ import sitemap from '@astrojs/sitemap';
 import { remarkExcerpt } from './src/plugins/remark-excerpt.mjs';
 import { remarkReadingTime } from './src/plugins/remark-reading-time.mjs';
 import remarkSectionize from 'remark-sectionize';
+import remarkGfm from 'remark-gfm';
 
 import { transformerCopyButton } from '@selemondev/shiki-transformer-copy-button';
 import { transformerColorizedBrackets } from '@shikijs/colorized-brackets';
@@ -31,7 +32,7 @@ export default defineConfig({
   site: "https://al-ghoul.github.io",
   integrations: [
     react(), mdx({
-      remarkPlugins: [remarkRelatedTitles, remarkExcerpt, remarkReadingTime, remarkSectionize],
+      remarkPlugins: [remarkRelatedTitles, remarkExcerpt, remarkReadingTime, remarkSectionize, remarkGfm],
     }),
     sitemap({
       i18n: {
